ae1604739f1926b3352513f3e374ed58f3500acf
ali423 <48999122+ali423@users.noreply.github.com>
2022-09-15 11:57:47 +0430

    complete passport auth and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        dd(auth()->check());
+        $data = $request->validated();
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            $response=['token' => $token];
+            return $this->responseSuccess($response);
+        } else {
+            return $this->responseError('کلمه عبور یا نام کاربری صحیح نیست');
+        }
+    }
+}
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function responseError($message = null, $status = 500)
+    {
+        $output = [
+            'success' => false,
+            'message' => $message,
+        ];
+        return response()->json($output, $status);
+    }
+    protected function responseSuccess($data = null, $status = 200)
+    {
+        $output = [
+            'success' => true,
+            'data' => $data,
+        ];
+        return response()->json($output, $status);
+    }
+}
